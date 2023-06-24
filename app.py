75ae3c0378e74d054e6b10d14a31945b3cb76eda
educunhamxk <92655692+educunhamxk@users.noreply.github.com>
2023-06-24 20:33:57 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pycaret
 from pycaret.regression import *
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
