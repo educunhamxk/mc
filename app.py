038a66cb3c28647e909e8d21468f1fbeb22ace27
educunhamxk <92655692+educunhamxk@users.noreply.github.com>
2023-06-24 20:59:34 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ if st.button('Carregar modelo e fazer previsão'):
     #criação do novo registro referente a projeção para ser empilhado na base principal
     novo_registro = {'name': pais, 'date': data, 'registro_projecao': 1, 'currency_code' : moeda}
     novo_registro['date'] = pd.to_datetime(novo_registro['date'], format='%d/%m/%Y')
-    df = pd.concat([df, novo_registro], ignore_index=True)
+    df = pd.concat([df, novo_registro])
 
     #preenchendo com 0s nas colunas que não são referentes ao país e nem a moeda do país
     df = df.fillna(0)
